29ac98af6c89bb2840d02be05e3a3f5e49e429ae
WendyYanto <yanto.wendy98@gmail.com>
2019-03-13 09:08:39 +0700

    Add Entity Validation

--- src/main/java/com/blibliproject/product/model/Product.java
+++ src/main/java/com/blibliproject/product/model/Product.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
@@ -15,15 +16,28 @@ import javax.persistence.*;
 public class Product {
 
     @Id
+    @NotBlank
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
+    @NotBlank
     private String name;
+
+    @NotBlank
     private float price;
+
+    @NotBlank
     private String main_image;
+
+    @NotBlank
     private String thumbnail_image;
+
+    @NotBlank
     private int quantity;
+
+    @NotBlank
     private int rating;
+
     private int CategoryId;
 
 }
